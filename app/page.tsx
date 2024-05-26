457a7654d2500d187bd6afd7d32a88fe86678144
fabienethevepro <fabienethevepro@gmail.com>
2024-05-26 15:45:19 +0000

    add three and drei

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
-
+"use client";
+import { Canvas } from "@react-three/fiber";
 export default function Home() {
   return (
     <main>
-      
+      <div className="h-screen w-full">
+        <Canvas>
+          <ambientLight intensity={0.5} />
+          <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+        </Canvas>
+      </div>
     </main>
   );
 }
